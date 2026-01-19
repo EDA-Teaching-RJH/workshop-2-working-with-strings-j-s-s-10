eb44c9dbbbc6189697ea7b5f7af3071ba2e0b2cd
j-s-s-10 <jss10@kent.ac.uk>
2026-01-19 13:46:29 +0000

    Update partThree.py

--- partThree.py
+++ partThree.py
@@ -6,9 +6,9 @@ def main():
 
 
 def pounds_to_float(d):
-    # TODO
+    return float(d.removeprefix("£"))
 
 def percent_to_float(p):
-    # TODO
+    return float(p.removesuffix("%")) / 100
 
 main()
