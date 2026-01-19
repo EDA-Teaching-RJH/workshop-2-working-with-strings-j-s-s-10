6e5f39197e36e3ff420a3f3bc89953a2f4581b31
j-s-s-10 <jss10@kent.ac.uk>
2026-01-19 13:31:24 +0000

    Update partTwo.py

--- partTwo.py
+++ partTwo.py
@@ -1,9 +1,9 @@
 import math  
 
 def main():
-#TO DO  
+    print(pythag(int(input("Enter a side length: ")), int(input("Enter the other side length: "))))
 
 def pythag(A,B):
-#TO DO  
+    return math.sqrt(A**2 + B**2)
 
 main()
